b15253d35f8e88f0d1846c1907831afcc9a0e77f
Adrian Eguez <eadepto@hotmail.com>
2020-01-06 16:05:40 -0500

    ejemplo con librerias

---
+++ 05-proyecto/index.js
@@ -0,0 +1,7 @@
+// NODE MODULES
+// const sumaLibreria = require('libreria-2019b/lib/suma');
+// Importar todo el contenido con el nombre 'sumaLibreria'
+// del paquete 'libreria-2019b/lib/suma'
+var sumaLibreria = require('libreria-2019b/lib/suma');
+var respuesta = sumaLibreria(1, 2);
+console.log('La respuesta es:', respuesta);
---
+++ 05-proyecto/index.ts
@@ -0,0 +1,12 @@
+// NODE MODULES
+// const sumaLibreria = require('libreria-2019b/lib/suma');
+
+// Importar todo el contenido con el nombre 'sumaLibreria'
+// del paquete 'libreria-2019b/lib/suma'
+import * as sumaLibreria from 'libreria-2019b/lib/suma';
+
+const respuesta = sumaLibreria(1,2);
+console.log('La respuesta es:', respuesta);
+
+
+
