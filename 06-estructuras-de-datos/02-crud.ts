ec04f9371b825df11649cab6a2ef229074612e25
Adrian Eguez <eadepto@hotmail.com>
2020-01-20 15:17:27 -0500

    sincrono vs asincrono

--- 06-estructuras-de-datos/02-crud.ts
+++ 06-estructuras-de-datos/02-crud.ts
@@ -1,10 +1,39 @@
 // 02-crud.ts create read update delete
 // const prompts = require('prompts');
 import * as prompts from 'prompts';
+import { RespuestaEdad } from './interfaces/respuesta-edad.interface';
+import { RespuestaNombre } from './interfaces/respuesta-nombre.interface';
 
 function main(){
-    obtenerDatosAnimalPerrito();
+    // npm uninstall -g tsc -> WINDOWS
+    // npm uninstall -g typescript -> WINDOWS
+    // npm install -g typescript -> WINDOWS
+    // sudo npm uninstall -g tsc -> LINUX O MAC
+    // sudo npm install -g typescript -> LINUX O MAC
+
+    // obtenerDatosAnimalPerrito(); // Asincrona
+    obtenerDatosAnimalPerritoSincrono()
+    .then()
+    .catch(); // -> PROMESA // Sincrona
 }
+
+async function obtenerDatosAnimalPerritoSincrono(){
+    console.log('Inicio');
+    const edad: RespuestaEdad = await prompts({
+        type: 'number',
+        name: 'edad',
+        message: 'Puedes darme tu edad?'
+    });
+    const nombre: RespuestaNombre = await prompts({
+        type: 'text',
+        name: 'nombre',
+        message: 'Puedes darme tu nombre?'
+    });
+    console.log('Edad', edad.edad);
+    console.log('Nombre', nombre.nombre);
+    console.log('Fin');
+}
+
 function obtenerDatosAnimalPerrito(){
     // Paradigma de programacion
     // SINCRONO vs ASINCRONO
@@ -48,3 +77,8 @@ function obtenerDatosAnimalPerrito(){
     console.log('Fin');
 }
 main();
+
+
+
+
+
